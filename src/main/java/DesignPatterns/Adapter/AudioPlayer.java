450d5e681ce126c111b882b4fd2ba2cf4610bfc3
Timothy John Manas <timothy.john.manas@gmail.com>
2017-04-08 17:37:43 -0400

    Created Adapter Design pattern Recipe

---
+++ src/main/java/DesignPatterns/Adapter/AudioPlayer.java
@@ -0,0 +1,37 @@
+package DesignPatterns.Adapter;
+
+/**
+ * Created by timmanas on 2017-04-08.
+ */
+public class AudioPlayer implements MediaPlayer {
+
+    private MediaAdapter mediaAdapter;
+
+    public AudioPlayer(){
+
+    }
+
+    public void play(String audioType, String fileName) {
+
+        switch(audioType){
+
+            case "mp3":{
+                System.out.println("Now playing MP3 song: " + fileName);
+                break;
+            }
+
+            case "mp4":
+            case "vlc":{
+                System.out.println("Using Adapter .... ");
+                mediaAdapter = new MediaAdapter(audioType);
+                mediaAdapter.play(audioType, fileName);
+                break;
+            }
+
+            default:{
+                System.out.println("Invalid type: " + fileName);
+            }
+        }
+
+    }
+}
