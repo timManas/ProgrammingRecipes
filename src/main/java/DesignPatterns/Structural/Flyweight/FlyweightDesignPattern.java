9b5419563eb261703aab5c29b57a1100cb515c6f
Timothy John Manas <timothy.john.manas@gmail.com>
2017-04-09 16:31:49 -0400

    Created Empty shell for Fly weight design pattern

---
+++ src/main/java/DesignPatterns/Structural/Flyweight/FlyweightDesignPattern.java
@@ -0,0 +1,14 @@
+package DesignPatterns.Structural.Flyweight;
+
+/**
+ * Created by timmanas on 2017-04-09.
+ */
+public class FlyweightDesignPattern {
+
+    public static void main(String[] args){
+        System.out.println("::: Flyweight Design Pattern :::");
+
+    }
+
+
+}
