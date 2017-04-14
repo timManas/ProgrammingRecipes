385099952f5928c75ee2ec0c156b41b8662ae58a
Timothy John Manas <timothy.john.manas@gmail.com>
2017-04-14 16:30:01 -0400

    Created Binary Heap Recipe

---
+++ src/main/java/Heaps/BinaryHeap/Main.java
@@ -0,0 +1,34 @@
+package Heaps.BinaryHeap;
+
+import java.util.Arrays;
+
+/**
+ * Created by timmanas on 2017-04-14.
+ */
+public class Main {
+
+    public static void main(String [] args){
+
+        Heap<String> heap = new Heap<>();
+        heap.insert("T");
+        heap.insert("i");
+        heap.insert("m");
+        heap.insert("o");
+        heap.insert("t");
+        heap.insert("h");
+        heap.insert("y");
+
+        System.out.println(heap);
+        heap.deleteMin();
+        System.out.println(heap);
+
+
+        Heap<Integer> intHeap = new Heap<>();
+        Integer[] a = {3,6,7,2,6,7,2,1,8,5,9};
+        intHeap.heapSort(a);
+        System.out.println(Arrays.toString(a));
+
+    }
+}
+
+
