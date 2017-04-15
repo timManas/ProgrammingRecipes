ccc22b5c80a3273769b0f6ea0e02467940f36fb3
Timothy John Manas <timothy.john.manas@gmail.com>
2017-04-15 18:40:46 -0400

    Added Scanner recipe for Java

---
+++ src/main/java/Misc/Scanner/ScannerDemo.java
@@ -0,0 +1,40 @@
+package Misc.Scanner;
+
+import java.util.Scanner;
+
+/**
+ * Created by timmanas on 2017-04-15.
+ */
+public class ScannerDemo {
+
+    public static void main(String [] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Name: " + name);
+
+        char gender = scanner.next().charAt(0);
+        System.out.println("Gender: " + gender);
+
+        int age = scanner.nextInt();
+        System.out.println("Age: " + age);
+
+        long mobileNumber = scanner.nextLong();
+        System.out.println("Mobile Number: " + mobileNumber);
+
+    }
+}
+
+
+/**
+
+ Scanner
+
+ Notes
+ - "System.in" ask Input from the user
+ - If the input does not match the type requested, an error will be produced ...DUH
+ - We can have multiple types to fetch
+ 
+ */
