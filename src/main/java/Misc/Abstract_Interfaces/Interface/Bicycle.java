fb1d094d21770b5ec2b67537b1ed63d5b7fb6a17
Timothy John Manas <timothy.john.manas@gmail.com>
2017-08-17 09:44:40 -0400

    Created Interface Example

---
+++ src/main/java/Misc/Abstract_Interfaces/Interface/Bicycle.java
@@ -0,0 +1,65 @@
+package Misc.Abstract_Interfaces.Interface;
+
+/**
+ * Created by timmanas on 2017-08-17.
+ */
+public class Bicycle implements Vehicle {
+
+    //region Variables
+    private int speed;
+    private int gear;
+    private String licensePlate;
+    //endregion
+
+    //region Constructor
+    public Bicycle() {
+        this.speed = 0;
+        this.gear = 1;
+        licensePlate = Vehicle.licensePlate;
+    }
+    //endregion
+
+    //region Getters
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    //endregion
+
+    //region Setters
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    //endregion
+
+    //region Helpers
+    @Override
+    public void changeGear() {
+        gear++;
+    }
+
+    @Override
+    public void speedUp() {
+        speed += 10;
+    }
+
+    @Override
+    public void applyBrakes() {
+        if(speed < 0)
+            speed -= 10;
+
+        if(gear < 1)
+            gear --;
+    }
+    //endregion
+
+}
---
+++ src/main/java/Misc/Abstract_Interfaces/Interface/Motorcycle.java
@@ -0,0 +1,66 @@
+package Misc.Abstract_Interfaces.Interface;
+
+/**
+ * Created by timmanas on 2017-08-17.
+ */
+public class Motorcycle implements Vehicle {
+
+    //region Variables
+    private int speed;
+    private int gear;
+    private String licensePlate;
+    //endregion
+
+    //region Constructor
+    public Motorcycle () {
+        this.speed = 0;
+        this.gear = 1;
+        licensePlate = Vehicle.licensePlate;
+    }
+    //endregion
+
+    //region Getters
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    //endregion
+
+    //region Setters
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    //endregion
+
+    //region Helpers
+    @Override
+    public void changeGear() {
+        gear++;
+    }
+
+    @Override
+    public void speedUp() {
+        speed += 20;
+    }
+
+    @Override
+    public void applyBrakes() {
+        if(speed > 0)
+            speed -= 20;
+
+        if(gear > 1)
+            gear --;
+    }
+    //endregion
+
+
+}
