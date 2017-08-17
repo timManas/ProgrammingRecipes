19a1ea013312496dbfdd2b9bcf676a0a11ce65ea
Timothy John Manas <timothy.john.manas@gmail.com>
2017-08-17 08:39:02 -0400

    Created initial implementation of a MultiThread

---
+++ src/main/java/MultiThreading/ThreadExtended.java
@@ -0,0 +1,16 @@
+package MultiThreading;
+
+import Misc.Synchronization.ThreadedSend;
+
+/**
+ * Created by timmanas on 2017-08-16.
+ */
+public class ThreadExtended extends Thread{
+
+    //region Helpers
+    public void run (){
+        System.out.println("Extended - Thread: " + Thread.currentThread().getId() + " is Running");
+    }
+    //endregion
+
+}
