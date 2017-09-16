3f42ac36a71ef9317c47e77975d5de42253ac7cc
Timothy John Manas <timothy.john.manas@gmail.com>
2017-09-16 15:07:03 -0400

    Created Map Data Structure for testing

--- src/main/java/DataStructures/List/ListExample.java
+++ src/main/java/DataStructures/List/ListExample.java
@@ -10,45 +10,21 @@ import java.util.*;
 public class ListExample {
 
     //region Members
-
+    private static List numericList1;
+    private static List numericList2;
+    private static List stringList1;
+    private static List stringList2;
     //endregion
 
     //region Main
     public static void main(String [] args) {
 
-        // Creating a List
-        List numericList1 = new ArrayList();
-        numericList1.add(1);
-        numericList1.add(3);
-        numericList1.add(9);
-        numericList1.add(4);
-        numericList1.add(8);
-
-        // Create  List using Generics
-        List stringList1 = new ArrayList<>();
-        stringList1.add("Hello");
-        stringList1.add("Mr.");
-        stringList1.add("Manas");
-        stringList1.add("You are the Best");
-
-
-        List numericList2 = new ArrayList();
-        numericList1.add(4);
-        numericList1.add(5);
-        numericList1.add(6);
-        numericList1.add(3);
-        numericList1.add(2);
-
-        // Create  List using Generics
-        List stringList2 = new ArrayList<>();
-        stringList1.add("Yo");
-        stringList1.add("Mr.");
-        stringList1.add("Tim");
-        stringList1.add("You are a Beast !!");
-
+        // Create a List
+        createList();
 
         // Iterator
         iterateList(numericList1);
+        iterateList(stringList1);
 
         // Comparator
         compareList(numericList1, numericList1);
@@ -57,26 +33,62 @@ public class ListExample {
         usefulMethods(numericList1, numericList1);
         usefulMethods(stringList1, numericList1);
 
-
-
     }
+
 
     //endregion
 
     //region Helpers
+
+    private static void createList() {
+
+        // Creating a List
+        numericList1 = new ArrayList();
+        numericList1.add(1);
+        numericList1.add(3);
+        numericList1.add(9);
+        numericList1.add(4);
+        numericList1.add(8);
+
+        // Create  List using Generics
+        stringList1 = new ArrayList<>();
+        stringList1.add("Hello");
+        stringList1.add("Mr.");
+        stringList1.add("Manas");
+        stringList1.add("You are the Best");
+
+
+        numericList2 = new ArrayList();
+        numericList2.add(4);
+        numericList2.add(5);
+        numericList2.add(6);
+        numericList2.add(3);
+        numericList2.add(2);
+
+        // Create  List using Generics
+        stringList2 = new ArrayList<>();
+        stringList2.add("Yo");
+        stringList2.add("Mr.");
+        stringList2.add("Tim");
+        stringList2.add("You are a Beast !!");
+
+    }
+
     private static void iterateList(List list) {
 
+        System.out.println("\n====== Iterating List ====== ");
+
         // Iterator through objects
-        System.out.println("\nIterate using Foor Loop");
+        System.out.println("\nIterate using For Loop");
         for(int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+            System.out.print(list.get(i));
         }
 
         // Iterate using while Loop
         System.out.println("\nIterate using While Loop");
         int i = 0;
         while (i < list.size()) {
-            System.out.println(list.get(i));
+            System.out.print(list.get(i));
             i++;
         }
 
@@ -84,19 +96,24 @@ public class ListExample {
         System.out.println("\nIterate using Iterator");
         Iterator iterator =  list.iterator();
         while(iterator.hasNext()) {
-            System.out.println(iterator.next());
+            System.out.print(iterator.next());
         }
 
         // Iterate using a sequential Stream from Collections.Utils
         System.out.println("\nIterate using Collections.Util");
-        list.forEach((temp) -> System.out.println(temp));
+        list.forEach((temp) -> System.out.print(temp));
 
     }
 
     private static void compareList(List list1, List list2) {
 
+        System.out.println("\n====== Comparing List ====== ");
+
+        System.out.println("\nOriginal List1: " + list1);
+        System.out.println("Original list2: " + list2);
+
         // Using the Equals
-        System.out.println("Using Equals comparator: " + list1.equals(list2));
+        System.out.println("\nUsing Equals comparator: " + list1.equals(list2));
 
         // Using the Deep Equals(list1, list2)
         Object [] arrayObj1 = list1.toArray();
@@ -120,6 +137,11 @@ public class ListExample {
 
 
     private static void usefulMethods(List list, List temp) {
+
+        System.out.println("\n====== Useful Methods ====== ");
+
+        System.out.println("Original List: " + list);
+        System.out.println("Original Temp: " + temp);
 
         // IndexOf - This works better if there are no duplicates
         System.out.println("\nIndexOf()");
---
+++ src/main/java/DataStructures/Map/MapExample.java
@@ -0,0 +1,204 @@
+package DataStructures.Map;
+
+import java.util.*;
+
+/**
+ * Created by timmanas on 2017-09-16.
+ */
+public class MapExample {
+
+    //region Members
+    private static Map map1;
+    private static Map map2;
+    private static Map map3;
+    private static HashMap map4;
+    private static LinkedHashMap linkedHashMap;
+    private static TreeMap treeMap;
+    //endregion
+
+    //region Main
+    public static void main(String [] args) {
+
+        // Create a Map
+        createMap();
+
+        // Iterator
+        iterateMap(linkedHashMap);
+        iterateMap(treeMap);
+
+        // Comparator
+        compareMap(map1, map1);
+
+        // Useful Methods
+        usefulMethods(linkedHashMap, treeMap);
+//        usefulMethods(map3, map4);
+
+    }
+
+
+    //endregion
+
+    //region Helpers
+    private static void createMap() {
+
+        // Create a HashMap the Old fashion way
+        map1 = new HashMap<Integer, String>();
+
+        // Create a NEW Object (Not a reference) HashMap with a predefined size
+        map2 = new HashMap(5);
+
+        map3 = new TreeMap<String, String>();
+
+        linkedHashMap = new LinkedHashMap<Integer, String>();
+        treeMap = new TreeMap<Integer, String>();
+
+        addElementsToMap();
+
+        // Create a HashMap from another Map
+        map4 = new HashMap(map1);
+    }
+
+    private static void addElementsToMap() {
+
+        map1.put(1, "Hello");
+        map1.put(4,"Mr.");
+        map1.put(5, "Tim");
+        map1.put(6, "We meet");
+        map1.put(2, "Again !!");
+
+        map2.put(8, "Go");
+        map2.put("Key1", "Forth");
+        map2.put(5, "&");
+        map2.put("Key2", "Conquer");
+        map2.put("Key3", null);
+
+        map3.put("Key1", "Value1");
+        map3.put("Key2", null);
+        map3.put("Key3", "Value3");
+        map3.put("Key4", "Value4");
+        map3.put("Key5", "Value5");
+
+        linkedHashMap.put(1, "Hello");
+        linkedHashMap.put(4,"Mr.");
+        linkedHashMap.put(5, "Tim");
+        linkedHashMap.put(6, "We meet");
+        linkedHashMap.put(2, "Again !!");
+
+        treeMap.put(1, "Bonjour");
+        treeMap.put(4,"Dr.");
+        treeMap.put(9, "Bond");
+        treeMap.put(6, "How would you like to");
+        treeMap.put(2, "Die !!");
+
+    }
+
+    private static void iterateMap(Map<Integer, String> map){
+
+        // Method#1 - Iterate over Entries using ForEach
+        System.out.println("\nIterating over Entries using ForEach");
+        for(Map.Entry<Integer, String> entry : map.entrySet())
+            System.out.println("Key: " + entry.getKey() + "     Value: " + entry.getValue());
+
+
+        // Method#2 - Iterating over key or Values using For Each
+        System.out.println("\nIterating over key or values using ForEach");
+        for(int key : map.keySet())
+            System.out.println("Key: " + key);
+
+        for(String values : map.values())
+            System.out.println("Values: " + values);
+
+        // Method#3 - Iterating using an Iterator
+        System.out.println("\nIterating using an Iterator");
+        Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.println("Key: " + entry.getKey() + "     Value: " + entry.getValue());
+        }
+    }
+
+    private static void compareMap(Map map1, Map map2) {
+
+        // Using the Equals Method
+        System.out.println("CompareMap :: Equals Method - Map1 & Map2: " + map1.equals(map2));
+
+        //Check each entry one by one
+        List<String> values1 = new ArrayList<String>(map1.values());
+        List<String> values2 = new ArrayList<String>(map2.values());
+        Collections.sort(values1);
+        Collections.sort(values2);
+        System.out.println("CompareMap :: Convert to List Method - Map1 & Map2: " + values1.equals(values2));
+
+
+    }
+
+    private static void usefulMethods(Map map1, Map map2) {
+
+        // clone
+        System.out.println("Cloning Map -- We dont use the clone(), since that is broken anyway");
+        LinkedHashMap tempLinkedHashMap = new LinkedHashMap(map1);
+
+        // containsKey
+        System.out.println("ContainsKey() :: map1 contain key '6': " + map1.containsKey(6));
+        System.out.println("ContainsKey() :: map1 contain key '1': " + map1.containsKey(1));
+        System.out.println("ContainsKey() :: map1 contain key '99': " + map1.containsKey(99));
+
+        // containsValue
+        System.out.println("ContainsValue() :: map1 contains value 'Again': " + map1.containsValue("Again !!"));
+        System.out.println("ContainsValue() :: map1 contains value 'Mr.': " + map1.containsValue("Mr."));
+        System.out.println("ContainsValue() :: map1 contains value 'Tim': " + map1.containsValue("Amanda"));
+
+        // get
+        System.out.println("get() ::: Getting the Value from Key '2': " + map1.get(2));
+
+        // keySet
+        System.out.println("\nKeySet :: Get List of All Kets");
+        for(Object key : map1.keySet())
+            System.out.println("Key: " + key);
+
+        // replace
+        System.out.println("Replace() ::: Replace key'5' from Tim to 'Romero': " + map1.replace(5, "Tim", "Romero"));
+        System.out.println("Replace() ::: Replace key'5' from Tim to 'Manas' (Should be false since Tim doesent Exist anymore): " + map1.replace(5, "Tim", "Manas"));
+        System.out.println("Replace() ::: Replace key'5'  to 'BadMon': " + map1.replace(5, "BadMon"));
+
+        // put
+        System.out.println("Put() ::: Put Key '99': " + map1.put(99, "For Realz ?"));
+
+        // putAll
+        System.out.println("PutAll() ::: Put all Values of map2 into map1 (Notice we replace & add values as well ? ");
+        map1.putAll(map2);
+
+        
+        // clear
+        System.out.println("Clearing Map");
+        map1.clear();
+        map2.clear();
+
+    }
+
+    //endregion
+
+
+}
+
+
+/**
+
+ Map
+
+ What ?
+ - Uses a key value pair to store
+ - No duplicate keys allowed
+ - Allows only 1 null Key / Value pair
+
+ Why ?
+
+ Notes
+ - Uses a HashTable to store to implement Map Interface
+ - Execution time of basic operation get/put to a remain constant even for large sets - Big O(1) - HOLY SHIT !!!!!
+ - HashMap do NOT store the order of the key/values
+ - LinkedHashMap will store the key/value pair in the order they were inserted
+ - TreeMap is ordered by the Key (implemented as Red Black Tree)
+ - HashTabble is synchronized
+ - PLEASE, PLEASE Specify the type of key value pair when Creating these bastards -__- . Ex.  new HashMap<Integer, String>()
+ */
